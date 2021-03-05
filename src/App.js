2fb29bcf237b8b5a00f2a867823a5ea27143ffd4
mdmurshed <mdmurshedul50@gmail.com>
2021-03-06 02:34:44 +0600

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+import Navbar from './components/Navbar'
+import ExercisesList from './components/ExercisesList'
+import EditExercises from './components/EditExercises'
+import CreateExercise from './components/CreateExercise'
+import CreateUser from './components/CreateUser'
 
 function App() {
   return (
-    <div className="App">
-      ok
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" exact component={EditExercises} />
+      <Route path="/exercises" exact component={CreateExercise} />
+      <Route path="/users" exact component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/EditExercises.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>you are on the Create exersice componeent</p>
+            </div>
+        )
+    }
+}
+
+export default EditExercises
