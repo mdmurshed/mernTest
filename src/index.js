a459ff1652317dcdcf7df9b0a106fe9c59424c80
mdmurshed <mdmurshedul50@gmail.com>
2021-03-07 00:54:36 +0600

    second1.1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   // <React.StrictMode>
     <App />
+     // 1 hour 22 min
   // </React.StrictMode>,
   ,document.getElementById('root')
 );
